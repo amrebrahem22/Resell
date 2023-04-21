19a6e5c10f4f5aa25899159ed71a7ad140289a42
unknown <amrebrahem226@gmail.com>
2023-04-21 18:35:52 +0200

    categories component

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -23,10 +23,18 @@ const UserMenu: React.FC<UserMenuPros> = ({ currentUser }) => {
     setOpen((prev) => !prev);
   }, []);
 
+  const onRent = useCallback(() => {
+    if(!currentUser) {
+      return loginModal.onOpen();
+    }
+
+    // open rent modal
+  }, [currentUser, loginModal]);
+
   return (
     <div className="relative">
       <div className="flex flex-row items-center gap-3">
-        <div className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer">
+        <div onClick={onRent} className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer">
           Resell your home
         </div>
         <div
